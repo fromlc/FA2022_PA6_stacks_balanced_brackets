9c02b651999ea5300a80afa62551943653a46fb1
L Carver <wifihum@gmail.com>
2022-10-13 18:41:19 -0700

    add string input loop

--- source/FA2022_PA6_stacks_balanced_brackets.cpp
+++ source/FA2022_PA6_stacks_balanced_brackets.cpp
@@ -11,8 +11,14 @@
 //------------------------------------------------------------------------------
 // using symbols
 //------------------------------------------------------------------------------
+using std::cin;
 using std::cout;
 using std::string;
+
+//------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+constexpr int MAX_LEN = 100;
 
 //------------------------------------------------------------------------------
 // globals
@@ -20,25 +26,26 @@ using std::string;
 static CharStack g_stack;
 
 static const enum g_brackets {
-    L_SQUARE = '[', 
-    R_SQUARE = ']',
-    L_CURLY = '{',
-    R_CURLY = '}',
-    L_PAREN = '(',
-    R_PAREN = ')',
+	L_SQUARE = '[',
+	R_SQUARE = ']',
+	L_CURLY = '{',
+	R_CURLY = '}',
+	L_PAREN = '(',
+	R_PAREN = ')',
 };
 
 //------------------------------------------------------------------------------
 // test bracket strings
 //------------------------------------------------------------------------------
-//static const string g_test = "[[{}]]";
-//static const string g_test = "[[{}]";
-static const string g_test = "[{}]]";
+//static const string g_test = "[[]{()}]";
+//static const string g_test = "()}";
+//static const string g_test = "[";
 
 //------------------------------------------------------------------------------
 // local function prototypes
 //------------------------------------------------------------------------------
-bool balancedBrackets();
+bool getInputString(string& s);
+bool balancedBrackets(string&);
 bool isMatchingRightBracket(char c);
 char cMatch(char c);
 
@@ -47,33 +54,53 @@ char cMatch(char c);
 //------------------------------------------------------------------------------
 int main() {
 
-    if (!balancedBrackets() || !g_stack.isEmpty())
-        cout << g_test << " is NOT balanced\n";
-    else
-        cout << g_test << " IS balanced\n";
+	string input;
+	while (getInputString(input)) {
+		if (!balancedBrackets(input) || !g_stack.isEmpty())
+			cout << input << " is NOT balanced\n";
+		else
+			cout << input << " IS balanced\n";
+
+		g_stack.makeEmpty();
+	}
+
+	cout << "\nGoodbye!\n";
+}
+
+//------------------------------------------------------------------------------
+// returns false if "q" or "Q" entered
+//------------------------------------------------------------------------------
+bool getInputString(string& s) {
+	cout << "\nEnter bracket string: ";
+	getline(cin, s);
+
+	bool test1 = (bool) !s.compare("q");
+	bool test2 = (bool) !s.compare("Q");
+
+	return !(test1 || test2);
 }
 
 //------------------------------------------------------------------------------
 // returns true if bracket string is balanced, false otherwise
 //------------------------------------------------------------------------------
-bool balancedBrackets() {
-    // assume balanced string
-    bool balanced = true;
+bool balancedBrackets(string& s) {
+	// assume balanced string
+	bool balanced = true;
 
-    for (char c : g_test) {
-        // push left bracket
-        if (c == L_SQUARE || c == L_CURLY || c == L_PAREN) {
-            g_stack.push(c);
-        }
-        // check for balance on right bracket
-        else if (c == R_SQUARE || c == R_CURLY || c == R_PAREN) {
-            balanced = isMatchingRightBracket(c);
-            if (!balanced) {
-                break;
-            }
-        }
-    }
-    return balanced;
+	for (char c : s) {
+		// push left bracket
+		if (c == L_SQUARE || c == L_CURLY || c == L_PAREN) {
+			g_stack.push(c);
+		}
+		// check for balance on right bracket
+		else if (c == R_SQUARE || c == R_CURLY || c == R_PAREN) {
+			balanced = isMatchingRightBracket(c);
+			if (!balanced) {
+				break;
+			}
+		}
+	}
+	return balanced;
 }
 
 //------------------------------------------------------------------------------
@@ -83,29 +110,29 @@ bool balancedBrackets() {
 // have a matching left bracket in the top of the stack
 //------------------------------------------------------------------------------
 bool isMatchingRightBracket(char c) {
-    if (g_stack.isEmpty())
-        return false;
+	if (g_stack.isEmpty())
+		return false;
 
-    if (cMatch(c) == g_stack.peek()) {
-        g_stack.pop();
-        return true;
-    }
+	if (cMatch(c) == g_stack.peek()) {
+		g_stack.pop();
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 //------------------------------------------------------------------------------
 // returns matching bracket character, or passed value as a last resort
 //------------------------------------------------------------------------------
 char cMatch(char c) {
-    // return matching left side
-    if (c == R_SQUARE) return L_SQUARE;
-    if (c == R_CURLY) return L_CURLY;
-    if (c == R_PAREN) return L_PAREN;
-    // return matching right side
-    if (c == L_SQUARE) return R_SQUARE;
-    if (c == L_CURLY) return R_CURLY;
-    if (c == L_PAREN) return R_PAREN;
-    // last resort returns passed value
-    return c;
+	// return matching left side
+	if (c == R_SQUARE) return L_SQUARE;
+	if (c == R_CURLY) return L_CURLY;
+	if (c == R_PAREN) return L_PAREN;
+	// return matching right side
+	if (c == L_SQUARE) return R_SQUARE;
+	if (c == L_CURLY) return R_CURLY;
+	if (c == L_PAREN) return R_PAREN;
+	// last resort returns passed value
+	return c;
 }
